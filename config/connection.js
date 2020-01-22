cd9a439dc773f4931e17d7b0916d684cc7fb215c
Marcos Luna <ml042685@yahoo.com>
2020-01-21 18:18:46 -0600

    eatalltacos

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 } else {
 	connection = mysql.createConnection({
 		host: 'localhost',
-		port:8080,
+		port:'3306',
 		user: 'root',
 		password: 'Avoionics@1985',
 		database: 'tacos_db'
